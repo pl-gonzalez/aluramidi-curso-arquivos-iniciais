d32e80580766ee31059d6f76c4828406c1f3a310
Leon Gonzalez <pleon256@gmail.com>
2023-10-18 17:29:05 -0300

    teste atualizar reposotorio local

--- main.js
+++ main.js
@@ -5,10 +5,5 @@ function tocaSomPom () {
 
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
-function tocaSomClap () {
-    document.querySelector('#som_tecla_clap').play();
 
-}
-
-document.querySelector('.tecla_clap').onclick = tocaSomClap;
 
